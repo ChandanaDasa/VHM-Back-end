07e6956d5c35ed1f152393a0afe1eb8183d69de0
b31user04 <b31user04@rpsconsulting.in>
2024-05-31 16:03:02 +0530

    backend VHM

---
+++ vehicleRegister.js
@@ -0,0 +1,31 @@
+const mangoose = require('mongoose');
+
+const vehicleRegister = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+    vehicleType: {
+        type: String,
+        required: true,
+    },
+    chassisNumber: {
+        type: String,
+        required: true,
+    },
+    vehicleModel: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+
+});
+module.exports = mongoose.model('vehicle', vehicleRegister);
